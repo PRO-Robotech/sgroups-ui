933ed65e0181f34dbe1c4142b7738d4aecb4e6f3
Pavel <pavel@MacBook-Air-Pavel.local>
2024-05-24 17:33:58 +0300

    framer motion arrows in editor

--- src/components/organisms/RulesEditor/populations/TransformBlock/constants.ts
+++ src/components/organisms/RulesEditor/populations/TransformBlock/constants.ts
@@ -11,8 +11,10 @@ export const SG_SG_IE_AND_SG_SG_IE_ICMP_TO_ID = 'SgSgIeAndSgSgIeIcmpTo'
 export const CIDR_TO_ID = 'CIDRTo'
 export const FQDN_TO_ID = 'FQDNTo'
 
-export const STROKE_COLOR = '#164893'
-export const STROKE_WIDTH = 2
+// export const STROKE_COLOR = '#164893'
+export const STROKE_COLOR = '#c3c3c3'
+export const STROKE_OPACITY = 0.6
+export const STROKE_WIDTH = 6
 
 export const ARROW_END_GAP = 20
 
