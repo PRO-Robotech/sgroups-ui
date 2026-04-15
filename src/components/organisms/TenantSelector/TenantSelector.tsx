49d8ada5728d20139a4b62862d9f5037f93ce9a6
typescreep <typescreep@prorobotech.ru>
2026-04-15 17:34:21 +0300

    hosts page wip

---
+++ src/components/organisms/TenantSelector/TenantSelector.tsx
@@ -0,0 +1,99 @@
+import React, { FC, useEffect, useMemo, useState } from 'react'
+import { Alert, Flex, Select, Typography } from 'antd'
+import { useLocation, useNavigate, useParams } from 'react-router-dom'
+import { useK8sSmartResource, TSingleResource } from '@prorobotech/openapi-k8s-toolkit'
+
+type TTenantSelectorProps = {
+  cluster?: string
+  tenant?: string
+}
+
+const ALL_TENANTS = '__all_tenants__'
+
+export const TenantSelector: FC<TTenantSelectorProps> = ({ cluster, tenant }) => {
+  const navigate = useNavigate()
+  const location = useLocation()
+  const params = useParams()
+  const [selectedTenant, setSelectedTenant] = useState<string | undefined>(tenant)
+
+  const {
+    data: tenantsData,
+    isLoading,
+    error,
+  } = useK8sSmartResource<{
+    items: TSingleResource[]
+  }>({
+    cluster: cluster || '',
+    apiGroup: 'sgroups.io',
+    apiVersion: 'v1alpha1',
+    plural: 'tenants',
+    isEnabled: Boolean(cluster),
+  })
+
+  useEffect(() => {
+    setSelectedTenant(tenant)
+  }, [tenant])
+
+  const options = useMemo(
+    () => [
+      { value: ALL_TENANTS, label: 'All Tenants' },
+      ...(tenantsData?.items
+        ?.map(item => item.metadata?.name)
+        .filter((value): value is string => Boolean(value))
+        .sort((a, b) => a.localeCompare(b))
+        .map(value => ({ value, label: value })) || []),
+    ],
+    [tenantsData],
+  )
+
+  const handleTenantChange = (value: string) => {
+    const nextTenant = value === ALL_TENANTS ? undefined : value
+    setSelectedTenant(nextTenant)
+
+    const parts = window.location.pathname.split('/').filter(Boolean)
+    const clusterParam = params.cluster
+
+    if (!clusterParam) {
+      return
+    }
+
+    const clusterIndex = parts.indexOf(clusterParam)
+    if (clusterIndex === -1) {
+      return
+    }
+
+    const namespaceIndex = clusterIndex + 1
+    const hasNamespace = Boolean(params.namespace)
+    const nextParts = [...parts]
+
+    if (nextTenant) {
+      if (hasNamespace) {
+        nextParts[namespaceIndex] = nextTenant
+      } else {
+        nextParts.splice(namespaceIndex, 0, nextTenant)
+      }
+    } else if (hasNamespace) {
+      nextParts.splice(namespaceIndex, 1)
+    }
+
+    navigate(`/${nextParts.join('/')}${location.search}`)
+  }
+
+  return (
+    <Flex vertical gap={8}>
+      <Flex gap={12} align="center" wrap>
+        <Typography.Text>Tenant:</Typography.Text>
+        <Select
+          placeholder="Tenant"
+          options={options}
+          value={selectedTenant || ALL_TENANTS}
+          onChange={handleTenantChange}
+          loading={isLoading}
+          disabled={!cluster}
+          style={{ minWidth: 260 }}
+        />
+      </Flex>
+      {error && <Alert type="error" message={`Failed to load tenants: ${String(error)}`} showIcon />}
+    </Flex>
+  )
+}
--- src/pages/HostsPage/HostsPage.tsx
+++ src/pages/HostsPage/HostsPage.tsx
@@ -1,5 +1,18 @@
-import React, { FC } from 'react'
-import { Greeting } from 'components'
+import React, { FC, useMemo } from 'react'
+import { Alert, Flex, Spin } from 'antd'
+import { AnyObject } from 'antd/es/_util/type'
+import { useSelector } from 'react-redux'
+import { EnrichedTable, TSingleResource, ContentCard, useK8sSmartResource } from '@prorobotech/openapi-k8s-toolkit'
+import { TenantSelector } from 'components'
+import { RootState } from 'store/store'
+import {
+  buildHostsColumns,
+  buildHostsDataSource,
+  HOSTS_KEY_TYPE_PROPS,
+  HOSTS_SORTERS_AND_FILTERS,
+  HOSTS_TRIM_LENGTHS,
+  HOSTS_UNDEFINED_VALUES,
+} from './tableConfig'
 
 type THostsPageProps = {
   cluster?: string
@@ -10,6 +23,51 @@ type THostsPageProps = {
   toggleTheme?: () => void
 }
 
-export const HostsPage: FC<THostsPageProps> = props => {
-  return <Greeting {...props} />
+export const HostsPage: FC<THostsPageProps> = ({ cluster, namespace }) => {
+  const theme = useSelector((state: RootState) => state.theme.theme)
+
+  const {
+    data: hostsData,
+    isLoading,
+    error,
+  } = useK8sSmartResource<{ items: TSingleResource[] }>({
+    cluster: cluster || '',
+    namespace,
+    apiGroup: 'sgroups.io',
+    apiVersion: 'v1alpha1',
+    plural: 'hosts',
+    isEnabled: Boolean(cluster),
+  })
+
+  const columns = useMemo(() => buildHostsColumns(), [])
+  const dataSource = useMemo(() => buildHostsDataSource(hostsData?.items || []), [hostsData?.items])
+
+  if (!cluster) {
+    return <Alert type="error" message="No cluster has been set" showIcon />
+  }
+
+  return (
+    <ContentCard displayFlex flexFlow="column" flexGrow={1}>
+      <Flex vertical gap={16}>
+        <TenantSelector cluster={cluster} tenant={namespace} />
+        {error && <Alert type="error" message={`Failed to load hosts: ${String(error)}`} showIcon />}
+        {isLoading && !hostsData && <Spin />}
+        {!error && hostsData && (
+          <EnrichedTable
+            theme={theme}
+            dataSource={dataSource as AnyObject[] | undefined}
+            columns={columns}
+            additionalPrinterColumnsUndefinedValues={HOSTS_UNDEFINED_VALUES}
+            additionalPrinterColumnsTrimLengths={HOSTS_TRIM_LENGTHS}
+            additionalPrinterColumnsKeyTypeProps={HOSTS_KEY_TYPE_PROPS}
+            additionalPrinterColumnsCustomSortersAndFilters={HOSTS_SORTERS_AND_FILTERS}
+            tableProps={{
+              disablePagination: false,
+            }}
+            withoutControls
+          />
+        )}
+      </Flex>
+    </ContentCard>
+  )
 }
